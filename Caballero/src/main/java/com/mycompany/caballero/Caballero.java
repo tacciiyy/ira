cff1d6ceb906e75861ce86f6452d3fc6d5a71fa6
tacciiyy <150228132+tacciiyy@users.noreply.github.com>
2023-11-08 15:25:02 +0800

    Add files via upload

---
+++ Caballero/src/main/java/com/mycompany/caballero/Caballero.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.caballero;
+
+/**
+ *
+ * @author WS
+ */
+public class Caballero {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
